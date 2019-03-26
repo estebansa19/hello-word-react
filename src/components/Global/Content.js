f2b2e70d4ce105aca413e2f65b635b07b050fdd7
Esteban Saldarriaga <esaldarriaga@ayenda.co>
2019-03-25 22:19:11 -0500

    First commit

---
+++ src/components/Global/Content.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './css/Content.css';
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="Content">
+        <h1>Soy el content</h1>
+        <p>Lorem</p>
+      </div>
+    );
+  }
+}
+
+export default Content;
